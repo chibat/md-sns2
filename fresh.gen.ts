d440b98a9cb63c2d9b37c99851587ddd20c12494
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2022-07-13 14:40:11 +0000

    modified:   fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,19 +3,63 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/counter.tsx";
-import * as $3 from "./routes/gfm.css.ts";
-import * as $4 from "./routes/index.tsx";
+import * as $1 from "./routes/api/auth_info.ts";
+import * as $2 from "./routes/api/callback.ts";
+import * as $3 from "./routes/api/create_comment.ts";
+import * as $4 from "./routes/api/create_follow.ts";
+import * as $5 from "./routes/api/create_like.ts";
+import * as $6 from "./routes/api/create_post.ts";
+import * as $7 from "./routes/api/delete_comment.ts";
+import * as $8 from "./routes/api/delete_follow.ts";
+import * as $9 from "./routes/api/delete_like.ts";
+import * as $10 from "./routes/api/delete_post.ts";
+import * as $11 from "./routes/api/get_comments.ts";
+import * as $12 from "./routes/api/get_follow_info.ts";
+import * as $13 from "./routes/api/get_follower_users.ts";
+import * as $14 from "./routes/api/get_following_users.ts";
+import * as $15 from "./routes/api/get_like_users.ts";
+import * as $16 from "./routes/api/get_liked_posts.ts";
+import * as $17 from "./routes/api/get_notifications.ts";
+import * as $18 from "./routes/api/get_post.ts";
+import * as $19 from "./routes/api/get_posts.ts";
+import * as $20 from "./routes/api/is_liked.ts";
+import * as $21 from "./routes/api/joke.ts";
+import * as $22 from "./routes/api/signout.ts";
+import * as $23 from "./routes/api/update_post.ts";
+import * as $24 from "./routes/counter.tsx";
+import * as $25 from "./routes/gfm.css.ts";
+import * as $26 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/counter.tsx": $2,
-    "./routes/gfm.css.ts": $3,
-    "./routes/index.tsx": $4,
+    "./routes/api/auth_info.ts": $1,
+    "./routes/api/callback.ts": $2,
+    "./routes/api/create_comment.ts": $3,
+    "./routes/api/create_follow.ts": $4,
+    "./routes/api/create_like.ts": $5,
+    "./routes/api/create_post.ts": $6,
+    "./routes/api/delete_comment.ts": $7,
+    "./routes/api/delete_follow.ts": $8,
+    "./routes/api/delete_like.ts": $9,
+    "./routes/api/delete_post.ts": $10,
+    "./routes/api/get_comments.ts": $11,
+    "./routes/api/get_follow_info.ts": $12,
+    "./routes/api/get_follower_users.ts": $13,
+    "./routes/api/get_following_users.ts": $14,
+    "./routes/api/get_like_users.ts": $15,
+    "./routes/api/get_liked_posts.ts": $16,
+    "./routes/api/get_notifications.ts": $17,
+    "./routes/api/get_post.ts": $18,
+    "./routes/api/get_posts.ts": $19,
+    "./routes/api/is_liked.ts": $20,
+    "./routes/api/joke.ts": $21,
+    "./routes/api/signout.ts": $22,
+    "./routes/api/update_post.ts": $23,
+    "./routes/counter.tsx": $24,
+    "./routes/gfm.css.ts": $25,
+    "./routes/index.tsx": $26,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
